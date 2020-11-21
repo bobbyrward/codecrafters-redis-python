bb8b851603a14d726e17a2465084e209fd430c1b
Bobby R. Ward <bobbyrward@gmail.com>
2020-11-20 23:14:48 -0600

    uncommented

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
-def main():
-    print("Your code goes here!")
+import socket
 
-    # Uncomment this to pass the first stage
-    #
-    # import socket
-    # s = socket.create_server(("localhost", 6379), reuse_port=True)
-    # s.accept() # wait for client
+
+def main():
+    s = socket.create_server(("localhost", 6379), reuse_port=True)
+    s.accept()  # wait for client
 
 
 if __name__ == "__main__":
